94410876dca9519ffa7d326bf804363921cebf75
Carlos Pires <extraterra1@gmail.com>
2023-09-04 20:06:04 +0100

    add exercises/palindromes

--- foundations/Javascript-Exercises/09_palindromes/palindromes.js
+++ foundations/Javascript-Exercises/09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  str = str
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9\s]/g, "")
+    .replaceAll(" ", "");
+  return str === str.split("").reverse().join("");
 };
 
 // Do not edit below this line
