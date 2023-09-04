603382a7bd79ec3339c4401e47d6ea84e56a9ad1
Carlos Pires <extraterra1@gmail.com>
2023-09-04 20:25:28 +0100

    add exercises/fibonacci

--- foundations/Javascript-Exercises/10_fibonacci/fibonacci.js
+++ foundations/Javascript-Exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 0) return "OOPS";
+  const fibonacci = [1, 1];
+  for (let i = 2; i < num; i++) {
+    fibonacci.push(fibonacci[i - 1] + fibonacci[i - 2]);
+  }
+  return fibonacci[num - 1];
 };
 
 // Do not edit below this line
