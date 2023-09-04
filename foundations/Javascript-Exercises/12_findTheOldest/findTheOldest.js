f801ec090df84617f3f35098b7de4fd6138d77d9
Carlos Pires <extraterra1@gmail.com>
2023-09-04 20:43:02 +0100

    add exercises/findTheOldest

--- foundations/Javascript-Exercises/12_findTheOldest/findTheOldest.js
+++ foundations/Javascript-Exercises/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  const peopleSorted = arr.sort((a, b) => {
+    if (!a.yearOfDeath) a.yearOfDeath = new Date().getFullYear();
+    if (!b.yearOfDeath) b.yearOfDeath = new Date().getFullYear();
+    console.log(a, b);
+    return b.yearOfDeath - b.yearOfBirth - (a.yearOfDeath - a.yearOfBirth);
+  });
 
+  return peopleSorted[0];
 };
 
 // Do not edit below this line
