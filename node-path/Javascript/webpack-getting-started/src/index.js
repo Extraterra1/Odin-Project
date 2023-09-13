c1baf3c7cac7fba5dc2b54b58b6c048af4f4af88
Carlos Pires <extraterra1@gmail.com>
2023-09-13 14:35:10 +0100

    add images,fonts

--- node-path/Javascript/webpack-getting-started/src/index.js
+++ node-path/Javascript/webpack-getting-started/src/index.js
@@ -1,6 +1,7 @@
 import _ from "lodash";
 import myName from "./myName";
 import "./style.css";
+import Img from "./img.png";
 
 function component() {
   const element = document.createElement("div");
@@ -9,6 +10,11 @@ function component() {
   element.innerHTML = _.join(["Hello", "webpack"], " ");
   element.innerHTML += myName("Joe");
   element.classList.add("hello");
+
+  const myImg = new Image();
+  myImg.src = Img;
+
+  element.appendChild(myImg);
 
   return element;
 }
