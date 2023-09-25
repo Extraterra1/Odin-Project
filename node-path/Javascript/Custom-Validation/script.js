d0cf344e9103f6f1536f0e4510230ecdca3735dc
Carlos Pires <extraterra1@gmail.com>
2023-09-25 18:29:10 +0100

    feat: validate password matching

--- node-path/Javascript/Custom-Validation/script.js
+++ node-path/Javascript/Custom-Validation/script.js
@@ -19,6 +19,22 @@ const checkZip = () => {
   if (previousError) previousError.remove();
 };
 
+const checkPass = () => {
+  const pass = document.querySelector('#pass');
+  const confirmPass = document.querySelector('#confirmPass');
+
+  if (pass.value !== confirmPass.value) {
+    showError(pass, 'Passwords must match');
+    return showError(confirmPass, 'Passwords must match');
+  }
+
+  [pass, confirmPass].forEach((e) => {
+    e.setCustomValidity('');
+    const previousError = e.parentNode.querySelector('span');
+    if (previousError) previousError.remove();
+  });
+};
+
 const showError = (el, error) => {
   el.setCustomValidity(error);
   const errMsg = document.createElement('span');
@@ -31,7 +47,7 @@ const showError = (el, error) => {
 const validate = function (evt) {
   if (this.name === 'email') return checkEmail();
   if (this.name === 'zip') return checkZip();
-  if (this.name.includes('pass')) return checkPass();
+  if (this.name.includes('ass')) return checkPass();
 };
 
 document.querySelectorAll('input').forEach((e) => e.addEventListener('blur', validate));
