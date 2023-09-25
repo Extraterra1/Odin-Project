a83395b967e9c19bd6b4ab2546ee50d225e16847
Carlos Pires <extraterra1@gmail.com>
2023-09-25 18:22:20 +0100

    feat: validate zip code

--- node-path/Javascript/Custom-Validation/script.js
+++ node-path/Javascript/Custom-Validation/script.js
@@ -10,6 +10,15 @@ const checkEmail = () => {
   if (previousError) previousError.remove();
 };
 
+const checkZip = () => {
+  const zip = document.querySelector('#zip');
+  if (zip.value.length !== 4) return showError(zip, 'Zip Code must be 4 digits long');
+
+  zip.setCustomValidity('');
+  const previousError = zip.parentNode.querySelector('span');
+  if (previousError) previousError.remove();
+};
+
 const showError = (el, error) => {
   el.setCustomValidity(error);
   const errMsg = document.createElement('span');
@@ -18,6 +27,14 @@ const showError = (el, error) => {
   if (previousError) previousError.remove();
   el.parentNode.insertBefore(errMsg, el.nextSibling);
 };
+
+const validate = function (evt) {
+  if (this.name === 'email') return checkEmail();
+  if (this.name === 'zip') return checkZip();
+  if (this.name.includes('pass')) return checkPass();
+};
+
+document.querySelectorAll('input').forEach((e) => e.addEventListener('blur', validate));
 
 form.addEventListener('submit', (evt) => {
   checkEmail();
