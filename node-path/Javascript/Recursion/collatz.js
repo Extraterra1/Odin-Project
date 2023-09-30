24197e3377f6c6b0dffb2315359d42bb0cadb247
Carlos Pires <extraterra1@gmail.com>
2023-09-30 18:47:21 +0100

    add power

--- node-path/Javascript/Recursion/collatz.js
+++ node-path/Javascript/Recursion/collatz.js
@@ -7,4 +7,11 @@ function collatz(n) {
   if (n % 2 !== 0) return collatz(n * 3 + 1);
 }
 
+function collatzNoCounter(n) {
+  if (n === 1) return 0;
+
+  if (n % 2 === 0) return 1 + collatz(n / 2);
+  if (n % 2 !== 0) return 1 + collatz(n * 3 + 1);
+}
+
 console.log(collatz(27));
