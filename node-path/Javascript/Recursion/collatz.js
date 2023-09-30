1c777042909465ad6005b689e1d808a3dafa0ce3
Carlos Pires <extraterra1@gmail.com>
2023-09-30 18:33:34 +0100

    fix collatz

--- node-path/Javascript/Recursion/collatz.js
+++ node-path/Javascript/Recursion/collatz.js
@@ -1,9 +1,10 @@
 let counter = 0;
 function collatz(n) {
-  console.log('operation ' + ++counter);
-  if (n === 1) return 1;
+  if (n === 1) return counter;
+  counter++;
+
   if (n % 2 === 0) return collatz(n / 2);
   if (n % 2 !== 0) return collatz(n * 3 + 1);
 }
 
-console.log(collatz(1543534534543534));
+console.log(collatz(27));
