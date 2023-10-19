1d2113f24fb47299d31d690ef6d4babf1c1f7566
Carlos Pires <extraterra1@gmail.com>
2023-10-19 17:51:40 +0100

    initial Person.jsx

--- node-path/React/my-first-react-app/src/Person.jsx
+++ node-path/React/my-first-react-app/src/Person.jsx
@@ -1,1 +1,24 @@
-export default function Person() {}
+import { useState } from 'react';
+
+export default function Person() {
+  const [person, setPerson] = useState({ name: 'John', age: 100 });
+
+  const handleIncreaseAge = () => {
+    setPerson({ ...person, age: person.age + 1 });
+  };
+
+  const handleNameChange = (data) => {
+    console.log(data);
+  };
+  return (
+    <>
+      <h1>{person.name}</h1>
+      <h2>{person.age}</h2>
+      <button onClick={handleIncreaseAge}>Increase age</button>
+      <label htmlFor="firstName">First Name</label>
+      <input type="text" onChange={handleNameChange} />
+      <label htmlFor="lastName">Last Name</label>
+      <input type="text" onChange={handleNameChange} />
+    </>
+  );
+}
