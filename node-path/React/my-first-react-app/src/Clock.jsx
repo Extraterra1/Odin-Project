36497779cd73c0f858264797a4c054c0978056d2
Carlos Pires <extraterra1@gmail.com>
2023-10-30 15:49:37 +0000

    import useState

--- node-path/React/my-first-react-app/src/Clock.jsx
+++ node-path/React/my-first-react-app/src/Clock.jsx
@@ -1,9 +1,16 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Clock() {
   const [time, setTime] = useState(0);
-  setTimeout(() => {
-    setTime((time) => time + 1);
-  }, 1000);
+  //   setTimeout(() => {
+  //     setTime((time) => time + 1);
+  //   }, 1000);
+
+  useEffect(() => {
+    setInterval(() => {
+      setTime((time) => time + 1);
+    }, 1000);
+  });
+
   return <h1>{time}</h1>;
 }
