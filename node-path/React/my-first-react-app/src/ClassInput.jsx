c13ca68ed84d3f0236c5bffdf1800204861c4e25
Carlos Pires <extraterra1@gmail.com>
2023-11-01 14:58:42 +0000

    feat: edit todo

--- node-path/React/my-first-react-app/src/ClassInput.jsx
+++ node-path/React/my-first-react-app/src/ClassInput.jsx
@@ -8,7 +8,11 @@ export default class ClassInput extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      todos: ['sample', 'lorem', 'ipsum'],
+      todos: [
+        { value: 'sample', editing: false, editingVal: 'sample' },
+        { value: 'lorem', editing: false, editingVal: 'sample' },
+        { value: 'ipsum', editing: false, editingVal: 'sample' }
+      ],
       inputVal: ''
     };
     this.handleInputChange = this.handleInputChange.bind(this);
@@ -25,16 +29,50 @@ export default class ClassInput extends Component {
   handleSubmit(e) {
     e.preventDefault();
     this.setState((state) => ({
-      todos: state.todos.concat(state.inputVal),
+      todos: state.todos.concat({ value: state.inputVal, editing: false }),
       inputVal: ''
     }));
   }
 
   handleDelete(todo) {
-    const newTodos = this.state.todos.filter((e) => e !== todo);
+    const newTodos = this.state.todos.filter((e) => e.value !== todo);
     this.setState((state) => {
       return { ...state, todos: newTodos };
     });
+  }
+
+  initiateEdit(todo) {
+    const newTodos = this.state.todos.map((e) => {
+      if (e.value !== todo.value) return e;
+      return {
+        ...e,
+        editing: !e.editing
+      };
+    });
+    this.setState((state) => ({ ...state, todos: newTodos }));
+  }
+
+  handleEditInputChange(todo, ev) {
+    const inputValue = ev.target.value;
+    const newTodos = this.state.todos.map((e) => {
+      if (e.value !== todo.value) return e;
+      return {
+        ...e,
+        editingVal: inputValue
+      };
+    });
+    this.setState((state) => ({ ...state, todos: newTodos }));
+  }
+
+  handleResubmit(todo) {
+    const newTodos = this.state.todos.map((e) => {
+      if (e.value !== todo.value) return e;
+      return {
+        value: e.editingVal,
+        editing: !e.editing
+      };
+    });
+    this.setState((state) => ({ ...state, todos: newTodos }));
   }
 
   render() {
@@ -58,12 +96,18 @@ export default class ClassInput extends Component {
         <ul>
           {this.state.todos.map((todo) => {
             return (
-              <li key={todo}>
-                <span className="todo-content">{todo}</span>
-                <span onClick={() => this.handleDelete(todo)} className="delete">
+              <li key={todo.value}>
+                {!todo.editing && <span className="todo-content">{todo.value}</span>}
+                {todo.editing && (
+                  <>
+                    <input onChange={(ev) => this.handleEditInputChange(todo, ev)} type="text" value={todo.editingVal} />
+                    <button onClick={() => this.handleResubmit(todo)}>Resubmit</button>
+                  </>
+                )}
+                <span onClick={() => this.handleDelete(todo.value)} className="delete">
                   <Icon icon="ion:trash-b" />
                 </span>
-                <span className="edit">
+                <span onClick={() => this.initiateEdit(todo)} className="edit">
                   <Icon icon="ion:edit" />
                 </span>
               </li>
