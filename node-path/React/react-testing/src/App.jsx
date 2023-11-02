71c5923bd5a50a37f511cba741d01588a62e5461
Carlos Pires <extraterra1@gmail.com>
2023-11-02 15:13:08 +0000

    button test

--- node-path/React/react-testing/src/App.jsx
+++ node-path/React/react-testing/src/App.jsx
@@ -1,10 +1,23 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 // import reactLogo from './assets/react.svg';
 // import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  return <h1>Our First Test</h1>;
+  const [heading, setHeading] = useState('Magnificent Monkeys');
+
+  const clickHandler = () => {
+    setHeading('Radical Rhinos');
+  };
+
+  return (
+    <>
+      <button type="button" onClick={clickHandler}>
+        Click Me
+      </button>
+      <h1>{heading}</h1>
+    </>
+  );
 }
 
 export default App;
