e1b37efb7cdde57c9bc859cb1797c46b65fc7d95
Carlos Pires <extraterra1@gmail.com>
2023-11-06 11:49:11 +0000

    set Counter proptypes

--- node-path/React/Prop-Types/src/Counter.jsx
+++ node-path/React/Prop-Types/src/Counter.jsx
@@ -1,5 +1,12 @@
+import PropTypes from 'prop-types';
+
 const Counter = ({ count, onClick }) => {
   return <h1 onClick={onClick}>Clicked {count} times</h1>;
+};
+
+Counter.propTypes = {
+  count: PropTypes.number,
+  onClick: PropTypes.func
 };
 
 export default Counter;
