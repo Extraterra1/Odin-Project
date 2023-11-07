4e92d85954985ecf4e2e1cbbc2ec73854db0afad
Carlos Pires <extraterra1@gmail.com>
2023-11-07 12:53:09 +0000

    implement routing

--- node-path/React/React-Router/src/Profile.jsx
+++ node-path/React/React-Router/src/Profile.jsx
@@ -4,6 +4,9 @@ const Profile = () => {
       <h1>This is a profile</h1>
       <h2>(It's a different route)</h2>
       <p>Nice to meet you</p>
+      <p>
+        <a href="/">Go Back</a>
+      </p>
     </div>
   );
 };
