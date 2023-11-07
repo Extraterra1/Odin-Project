b8b31bef075b0ea48a9c3353d13c4671d2e72399
Carlos Pires <extraterra1@gmail.com>
2023-11-07 12:11:52 +0000

    main page

--- node-path/React/React-Router/src/App.jsx
+++ node-path/React/React-Router/src/App.jsx
@@ -1,9 +1,23 @@
 // import { useState } from 'react';
-import Profile from './Profile';
+// import Profile from './Profile';
 import './App.css';
 
 function App() {
-  return <Profile />;
+  return (
+    <div>
+      <h1>This is the main page</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat a quam natus. Repellat nisi ad aspernatur officiis quae? Eum aliquid distinctio animi
+        dolorem libero vel unde ducimus sit et atque.
+      </p>
+      <nav>
+        <ul>
+          <li>
+            <a href="profile">Go to Profile</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
-
 export default App;
--- node-path/React/React-Router/src/Profile.jsx
+++ node-path/React/React-Router/src/Profile.jsx
@@ -2,6 +2,7 @@ const Profile = () => {
   return (
     <div>
       <h1>This is a profile</h1>
+      <h2>(It's a different route)</h2>
       <p>Nice to meet you</p>
     </div>
   );
