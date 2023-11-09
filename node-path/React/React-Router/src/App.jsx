08540c0b31b0bd8182593e80521ed8910d45c093
Carlos Pires <extraterra1@gmail.com>
2023-11-09 14:48:32 +0000

    error handling

--- node-path/React/React-Router/src/App.jsx
+++ node-path/React/React-Router/src/App.jsx
@@ -6,19 +6,29 @@ import './App.css';
 
 function App() {
   const [img, setImg] = useState({});
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      const res = await fetchJSON('https://jsonplaceholder.typicode.com/photos');
-      setImg({ url: res[0].url, title: res[0].title });
+      try {
+        const res = await fetchJSON('https://jsonplacehsssolder.typicode.com/photos');
+        setImg({ url: res[0].url, title: res[0].title });
+      } catch (error) {
+        setError(error);
+      }
     };
     fetchData();
   }, []);
 
   return (
     <div>
-      <img src={img.url} alt="" />
-      <h4>{img.title}</h4>
+      {error && <p>{error.message}</p>}
+      {img.url && (
+        <>
+          <img src={img.url} alt={img.title} />
+          <h4>{img.title}</h4>
+        </>
+      )}
       <h1>This is the main page</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat a quam natus. Repellat nisi ad aspernatur officiis quae? Eum aliquid distinctio animi
--- node-path/React/React-Router/src/helpers/fetchJSON.js
+++ node-path/React/React-Router/src/helpers/fetchJSON.js
@@ -2,8 +2,8 @@ export default async (url, options = {}) => {
   try {
     const res = await fetch(url, { mode: 'cors', ...options });
     if (res.ok) return res.json();
-    throw new Error('City not found');
-  } catch (err) {
-    console.log(err.message);
+    throw new Error('Failed to fetch');
+  } catch (error) {
+    throw new Error('Failed to fetch');
   }
 };
