632d951b6b1ccfdeb103bd465f28353b0dfbd417
Carlos Pires <extraterra1@gmail.com>
2023-11-09 14:29:27 +0000

    add imgurl state + useeffect

--- node-path/React/React-Router/src/App.jsx
+++ node-path/React/React-Router/src/App.jsx
@@ -1,11 +1,16 @@
-// import { useState } from 'react';
+import { useState, useEffect } from 'react';
 // import Profile from './Profile';
 import { Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
+  const [imgUrl, setImgUrl] = useState(null);
+
+  useEffect(() => {}, []);
+
   return (
     <div>
+      <img src="" alt="" />
       <h1>This is the main page</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat a quam natus. Repellat nisi ad aspernatur officiis quae? Eum aliquid distinctio animi
