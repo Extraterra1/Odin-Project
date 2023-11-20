8982533d30947d782c2508917d55035a3af91455
Carlos Pires <extraterra1@gmail.com>
2023-11-20 15:07:29 +0000

    prop types

--- node-path/React/Context/src/Title.jsx
+++ node-path/React/Context/src/Title.jsx
@@ -1,3 +1,11 @@
-export default function Title({ children }) {
+import PropTypes from 'prop-types';
+
+function Title({ children }) {
   return <h1>{children}</h1>;
 }
+
+Title.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]).isRequired
+};
+
+export default Title;
