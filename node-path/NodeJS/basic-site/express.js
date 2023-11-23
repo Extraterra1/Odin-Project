e163df79543940bded9e1f0a44f50514726c96a7
Carlos Pires <extraterra1@gmail.com>
2023-11-23 14:32:00 +0000

    about route express

--- node-path/NodeJS/basic-site/express.js
+++ node-path/NodeJS/basic-site/express.js
@@ -8,6 +8,9 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/about', (req, res) => {
+  res.render('about');
+});
 
 app.listen(port, () => {
   console.log('listening');
