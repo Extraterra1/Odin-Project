61227c2c849fe9f389c8aea9fbd6cefa456d0899
Carlos Pires <extraterra1@gmail.com>
2023-11-29 16:36:55 +0000

    db connection

--- node-path/NodeJS/Library-Site/app.js
+++ node-path/NodeJS/Library-Site/app.js
@@ -1,15 +1,19 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
 require('dotenv').config();
+
+const app = express();
+const dbUrl = process.env.DB_URL;
+mongoose.set('strictQuery', false);
+mongoose.connect(dbUrl);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
