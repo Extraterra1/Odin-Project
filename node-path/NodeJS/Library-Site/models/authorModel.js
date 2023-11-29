486b27799cfffc0e3fb8859f420d26a754a76383
Carlos Pires <extraterra1@gmail.com>
2023-11-29 16:43:39 +0000

    author schema fix

--- node-path/NodeJS/Library-Site/models/authorModel.js
+++ node-path/NodeJS/Library-Site/models/authorModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const Author = new Schema({
+const AuthorSchema = new Schema({
   first_name: { type: String, required: true, maxLength: 100 },
   family_name: { type: String, required: true, maxLength: 100 },
   date_of_birth: { type: Date },
@@ -27,4 +27,4 @@ AuthorSchema.virtual('url').get(function () {
   return `/catalog/author/${this._id}`;
 });
 
-module.exports = Author;
+module.exports = mongoose.model('Author', AuthorSchema);
