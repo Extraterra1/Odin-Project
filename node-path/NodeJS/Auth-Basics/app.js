b15a980bcc792079445530b9ea370e1ddcebc31b
Carlos Pires <extraterra1@gmail.com>
2023-12-18 11:34:12 +0000

    db connection + user model

--- node-path/NodeJS/Auth-Basics/app.js
+++ node-path/NodeJS/Auth-Basics/app.js
@@ -5,3 +5,26 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').LocalStrategy;
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+require('dotenv').config();
+mongoose.connect(process.env.DB_URL);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongo connection error'));
+
+const User = mongoose.model(
+  'User',
+  new Schema({
+    username: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
+  })
+);
+
+const app = express();
+app.set('views', __dirname);
+app.set('view engine', 'pug');
