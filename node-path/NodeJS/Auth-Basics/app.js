42806cb7e1a31ac28c2ff9cb98d9c1bd300cad69
Carlos Pires <extraterra1@gmail.com>
2023-12-18 12:50:33 +0000

    signup logic

--- node-path/NodeJS/Auth-Basics/app.js
+++ node-path/NodeJS/Auth-Basics/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
 const session = require('express-session');
+const asyncHandler = require('express-async-handler');
 const passport = require('passport');
-const LocalStrategy = require('passport-local').LocalStrategy;
+const LocalStrategy = require('passport-local').Strategy;
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -28,8 +29,33 @@ const User = mongoose.model(
 const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
 app.use(session({ secret: 'liandrys anguish', resave: false, saveUninitialized: true }));
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne(username);
+      if (!user) return done(null, false, { message: 'Incorrect username' });
+      if (user.password !== passwrod) return done(null, false, { message: 'Incorrect password' });
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
@@ -41,5 +67,16 @@ app.get('/', (req, res) => {
 app.get('/signUp', (req, res) => {
   res.render('signUp');
 });
+app.post(
+  '/signUp',
+  asyncHandler(async (req, res) => {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.pass
+    });
+    const result = await user.save();
+    res.redirect('/');
+  })
+);
 
 app.listen(3000, () => console.log('listening on port 3000'));
