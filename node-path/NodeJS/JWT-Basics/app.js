4d4a4d382f78e87ffa00e560cdffea7f4d971ee3
Carlos Pires <extraterra1@gmail.com>
2023-12-28 12:15:26 +0000

    final

--- node-path/NodeJS/JWT-Basics/app.js
+++ node-path/NodeJS/JWT-Basics/app.js
@@ -3,15 +3,36 @@ const jwt = require("jsonwebtoken");
 
 const app = express();
 
+const setToken = (req, res, next) => {
+  // Verify token
+  // Token format is "Bearer <token>"
+  // Get auth header
+  const bearerHeader = req.headers.authorization;
+
+  // Check if bearer is undefined
+  if (typeof bearerHeader === "undefined") return res.sendStatus(403);
+
+  // Get token from string
+  const bearerToken = bearerHeader.split(" ")[1];
+
+  // Set the token
+  req.token = bearerToken;
+  next();
+};
+
 app.get("/api", (req, res) => {
   res.json({
     message: "Welcome",
   });
 });
 
-app.post("/api/posts", isLoggedIn, (req, res) => {
-  res.json({
-    message: "Post created",
+app.post("/api/posts", setToken, (req, res) => {
+  jwt.verify(req.token, "cyberpunk 2077", (err, authData) => {
+    if (err) return res.status(403).json(err);
+    res.json({
+      message: "Post created",
+      authData,
+    });
   });
 });
 
@@ -27,11 +48,5 @@ app.post("/api/login", (req, res) => {
     res.json({ token });
   });
 });
-
-const isLoggedIn = (req, res, next) => {
-  // Verify token
-  // Get auth header
-  const bearerHeader = req.headers.authorization;
-};
 
 app.listen(3000, () => console.log("listening on 3000"));
