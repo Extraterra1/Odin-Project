f04a61535b20d6812d40c016cb9510c1b36fe9b7
Carlos Pires <extraterra1@gmail.com>
2023-12-31 11:15:43 +0000

    use isAuthenticated instead of auth

--- node-path/NodeJS/React-Auth-Kit/src/components/LogIn.jsx
+++ node-path/NodeJS/React-Auth-Kit/src/components/LogIn.jsx
@@ -1,33 +1,27 @@
-import { useSignIn, useAuthUser } from 'react-auth-kit';
+import { useSignIn, useIsAuthenticated } from 'react-auth-kit';
 import { Navigate } from 'react-router-dom';
 import axios from 'axios';
 import Header from './Header';
 
 const LogIn = () => {
   const signIn = useSignIn();
-  const auth = useAuthUser();
+  const isAuthenticated = useIsAuthenticated();
   const onLogin = async () => {
     try {
       const res = await axios.post('http://localhost:3000/api/login');
-      if (
-        signIn({
-          token: res.data.token,
-          tokenType: 'Bearer',
-          authState: res.data.user,
-          expiresIn: 60
-        })
-      ) {
-        console.log('good');
-      } else {
-        console.log('no good');
-      }
+      signIn({
+        token: res.data.token,
+        tokenType: 'Bearer',
+        authState: res.data.user,
+        expiresIn: 60
+      });
     } catch (err) {
       console.log(err);
     }
   };
   return (
     <>
-      {auth() && <Navigate to="/?logged" />}
+      {isAuthenticated() && <Navigate to="/?logged" />}
       <Header />
       <button onClick={onLogin}>Log In</button>
     </>
